66336372fffe932dbec97b4a35b6d1bc862ddf73
Devine-ops <vinigabrielsoa@gmail.com>
2024-07-17 20:22:49 -0300

    :construction: start footer modfication

--- src/components/ComponentRodaPe.vue
+++ src/components/ComponentRodaPe.vue
@@ -2,6 +2,7 @@
     <footer>
             <div class="infos">
                 <h4>Todos os direitos reservados - 2024</h4>
+                <h4>Desenvolvido por - Vinicius</h4>]
                 <h4>Desenvolvido por - Vinicius</h4>
             </div>
             <img class="logo-footer" src="../assets/logo_truck_me_frete_plataforma.webp">
